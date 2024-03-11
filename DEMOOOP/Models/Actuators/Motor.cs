bc00d802cf494349b69bc36090d0ec25f4543bb8
libbeyc22 <122319337+libbeyc22@users.noreply.github.com>
2024-03-11 13:07:40 -0400

    Add files via upload

---
+++ DEMOOOP/Models/Actuators/Motor.cs
@@ -0,0 +1,13 @@
+﻿namespace DEMOOOP.Models.Actuators;
+
+using DEMOOOP.Models.Sensors;
+
+public class Motor
+{
+    public Encoder Encoder { get; set; } = new();
+    public void Move(float speed)
+    {
+        // Move the motor at the given speed
+        Console.WriteLine($"Motor is moving at {speed} speed");
+    }
+}
